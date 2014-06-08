0e95daab70d003bdffc8605183dde625c09987b8
avamark <avamark@gmail.com>
2014-06-08 07:10:37 +0300

    1.0.0.3 	-- plot2 added

--- plot2.R
+++ plot2.R
@@ -11,16 +11,27 @@ rm(data)
 ds<- subset(ds,ds$dtf<"2007-02-02")
 wday<- data.frame(wday=weekdays(ds$dtf,T))
 ds<- data.frame(wday=wday,ds)
-
+ds[1]<- lapply(ds[1],as.factor)
+#add dateTime column to ds
+dtime<-strptime(paste(ds$Date, ds$Time), "%d/%m/%Y %H:%M:%S")
+ds<- data.frame(dtime=dtime,ds)
 # 
-
-hist(ds$Global_active_power,col="red",
-     xlab="Global Active Power (Kilowatts)",
-     main="Global Active Power"
+#plot on screen
+with(ds[ds$dtf>"2007-01-03" & ds$dtf<"2007-01-06"
+        ,c("dtime","Global_active_power")]
+     ,plot(dtime,Global_active_power,type="l"
+           ,ylab="Global Active Power (Kilowatts)"
+           ,xlab=""
+           )
+    )
+#plot on png file
+png(file = "plot2.png",,width=480,height=480,units="px")
+with(ds[ds$dtf>"2007-01-03" & ds$dtf<"2007-01-06"
+        ,c("dtime","Global_active_power")]
+     ,plot(dtime,Global_active_power,type="l"
+           ,ylab="Global Active Power (Kilowatts)"
+           ,xlab=""
+           )
      )
-png(file = "plot1.png", bg = "transparent",width=480,height=480,units="px")
-hist(ds$Global_active_power,col="red",
-     xlab="Global Active Power (Kilowatts)",
-     main="Global Active Power"
-)
+
 dev.off()
